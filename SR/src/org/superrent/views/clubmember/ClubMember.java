b2e048a70362e4cd81c8b2e7f07e7c8e90ac89f8
elmali <leevalve@gmail.com>
2014-04-10 23:52:23 -0700

    frame size

--- SR/src/org/superrent/views/clubmember/ClubMember.java
+++ SR/src/org/superrent/views/clubmember/ClubMember.java
@@ -53,7 +53,7 @@ public class ClubMember extends JFrame {
 		clubMemberController = new ClubMemberController(this);
 		lc = new LoginController(this);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 798, 514);
+		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
