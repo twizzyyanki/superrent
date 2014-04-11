a3841252277f0184ee19f8fc502a20d9d8f95b70
Sky <dtlsky@gmail.com>
2014-04-10 20:35:22 -0700

    some change for dao

--- SR/src/org/superrent/daos/saDAO.java
+++ SR/src/org/superrent/daos/saDAO.java
@@ -2,6 +2,11 @@ package org.superrent.daos;
 
 import java.sql.Connection;
 
+/**
+ * 
+ * This class is the DAO to load information for a system administrator 
+ *
+ */
 public class saDAO {
 	private Connection connection = null; 
 	private int uid;
